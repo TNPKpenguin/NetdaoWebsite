5f5d30d797ec05dd347cb590ef08fd7f348886b3
TNPKpenguin <64346499+TNPKpenguin@users.noreply.github.com>
2023-08-20 13:07:40 +0700

    Add files via upload

---
+++ includes/edit_history_file.php
@@ -0,0 +1,30 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $hn = $_POST["hn"];
+    $his = $_POST["his"];
+    $fam_his = $_POST["fam_his"];
+    $food_his = $_POST["food_his"];
+    $drug_his = $_POST["drug_his"];
+    $disease_his = $_POST["disease_his"];
+
+    try{
+        require_once "../includes/bdh.inc.php";
+        
+        $query = "UPDATE patient SET his_sick = ?, his_fam_sick = ?, food_allergy = ?, drug_allergy = ?, congenital_disease = ? WHERE hn = ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$his, $fam_his, $food_his, $drug_his, $disease_his, $hn]);
+
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../MeasureH2.php?hn=".$_POST['hn']);
+        die();
+    }catch(PDOException $e){
+        die("Query failed: ".$e->getMessage());
+    }
+    
+}else{
+    // header("Location: ../add_patient.php");
+    echo "error";
+}
---
+++ includes/edit_measure_file.php
@@ -0,0 +1,35 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    setlocale(LC_TIME, 'th_TH');
+    $hn = $_POST["hn"];
+    $date_time = date("Y-m-d H:i:s");
+    $date_time_thai = date("Y-m-d H:i:s", strtotime($date_time) + 5 * 3600);
+    $weight = $_POST["weight"];
+    $height = $_POST["height"];
+    $temp = $_POST["temp"]; 
+    $blood_pressure = $_POST["blood_pressure"];
+    $pulse = $_POST["pulse"];
+    $breath = $_POST["breath"];
+    
+    try{
+        require_once "bdh.inc.php";
+
+        $query = "UPDATE bio_information SET date_measure=?, weight=?, height=?, pulse=?,	breath=?,	temperature=?, blood_pressure=? WHERE hn = ?";
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->execute([$date_time_thai, $weight, $height, $pulse, $breath,  $temp, $blood_pressure,  $hn]);
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../Treatment.php?hn=".$_POST['hn']);
+        die();
+    }catch(PDOException $e){
+        die("Query failed: ".$e->getMessage());
+    }
+    
+}else{
+    // header("Location: ../add_patient.php");
+    echo "error";
+}
